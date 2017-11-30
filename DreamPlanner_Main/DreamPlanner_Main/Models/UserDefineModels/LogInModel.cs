41406a2acac849f6c3bdf6bcefc7c28146cfd163
RohanArif <rohanarif77@gmail.com>
2017-11-30 20:33:31 +0100

    Created Log in Model and add necessary fields in View Page.

---
+++ DreamPlanner_Main/DreamPlanner_Main/Models/UserDefineModels/LogInModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DreamPlanner_Main.Models.UserDefinedModels
+{
+    public class LogInModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
