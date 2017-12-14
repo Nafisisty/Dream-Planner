bdcf0d3309eca22eb851c5eff66f1c80d884792c
RohanArif <rohanarif77@gmail.com>
2017-12-15 00:56:16 +0100

    Add Rating in UserDefinedModels

---
+++ DreamPlanner_Main/DreamPlanner_Main/Models/UserDefinedModels/Rating.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DreamPlanner_Main.Models.UserDefinedModels
+{
+    public class Rating
+    {
+        public int RatingId { get; set; }
+        public string RatingName { get; set; }
+
+        
+    }
+}
