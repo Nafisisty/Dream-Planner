e8c7572e1ae3133f4910876409b5b346b3cdcd75
RohanArif <rohanarif77@gmail.com>
2017-12-15 00:59:09 +0100

    Change in Rating File UserExperience

--- DreamPlanner_Main/DreamPlanner_Main/Models/UserDefinedModels/Rating.cs
+++ DreamPlanner_Main/DreamPlanner_Main/Models/UserDefinedModels/Rating.cs
@@ -10,6 +10,6 @@ namespace DreamPlanner_Main.Models.UserDefinedModels
         public int RatingId { get; set; }
         public string RatingName { get; set; }
 
-        
+        public List<UserExperience> UserExperiences { get; set; }
     }
 }
