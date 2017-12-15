35fcbbd75f49f5e4e7d24bc95e15f9f638cad59c
RohanArif <rohanarif77@gmail.com>
2017-12-15 01:04:34 +0100

    Create Structure of User Experience

---
+++ DreamPlanner_Main/DreamPlanner_Main/Models/UserDefinedModels/UserExperience.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DreamPlanner_Main.Models.UserDefinedModels
+{
+    public class UserExperience
+    {
+       
+
+    }
+}
